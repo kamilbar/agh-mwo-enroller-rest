99999a3901dbff91793f3bc00631d90d5f3af175
Kamil Barcik <kbarcik@student.agh.edu.pl>
2021-04-24 11:50:11 +0200

    Changes implemented during laboratory

--- src/main/java/com/company/enroller/persistence/ParticipantService.java
+++ src/main/java/com/company/enroller/persistence/ParticipantService.java
@@ -2,6 +2,8 @@ package com.company.enroller.persistence;
 
 import java.util.Collection;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.springframework.stereotype.Component;
 
 import com.company.enroller.model.Participant;
@@ -10,13 +12,32 @@ import com.company.enroller.model.Participant;
 public class ParticipantService {
 
 	DatabaseConnector connector;
-
+	Session session;
+	
 	public ParticipantService() {
-		connector = DatabaseConnector.getInstance();
+//		connector = DatabaseConnector.getInstance();
+		session = DatabaseConnector.getInstance().getSession();
 	}
 
 	public Collection<Participant> getAll() {
-		return connector.getSession().createCriteria(Participant.class).list();
+//		return connector.getSession().createCriteria(Participant.class).list();
+		return session.createCriteria(Participant.class).list();
+	}
+
+	public Participant findByLogin(String login) {
+		return (Participant) session.get(Participant.class, login);
+	}
+
+	public void add(Participant participant) {
+		Transaction transaction = this.connector.getSession().beginTransaction();
+		session.save(participant);
+		transaction.commit();
+	}
+
+	public void delete(Participant participant) {
+		Transaction transaction = this.connector.getSession().beginTransaction();
+		session.delete(participant);
+		transaction.commit();
 	}
 
 }
